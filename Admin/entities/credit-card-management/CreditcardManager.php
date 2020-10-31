3c981c9ea70d1681f11bf7c56bb00da8be6d33d6
HOSTNAME47 <mouadstev1@gmail.com>
2020-10-31 12:11:30 +0100

    add and delete credit card type features

--- Admin/entities/credit-card-management/CreditcardManager.php
+++ Admin/entities/credit-card-management/CreditcardManager.php
@@ -24,6 +24,26 @@
             }
         }
 
+        function deleteCreditCard($id) {
+            $query = $this->link->prepare("DELETE FROM creditcardtype WHERE id = (:c_c_id)");
+            $query->bindParam(":c_c_id", $id);
+
+            $query->execute();
+        }
+
+        function editCreditCardType($id, $newName) {
+            if($this->idExists($id)) {
+                $query = $this->link->prepare("UPDATE creditcardtype SET creditCardType = (:c_c_type) where id = :id");
+                $query->bindParam(":id", $id);
+                $query->bindParam(":c_c_type", $newName);
+
+                $query->execute();
+                $counts = $query->rowCount();
+                
+                return $counts;
+            }
+        }
+
         function checkCreditCardTypeExistence($c_c_type) {
             $cat_name = $this->cleanData($c_c_type);
             
@@ -38,18 +58,40 @@
                 return false;
         }
 
+        function idExists($id) {
+            $query = $this->link->prepare("SELECT * FROM creditcardtype WHERE id = (:id)");
+            $query->bindParam(":id", $id);
+
+            $query->execute();
+            $counts = $query->rowCount();
+            
+            return ($counts > 0) ? true : false;
+        }
+
         function getCreditCardTypesAsComponents() {
             $query = $this->link->prepare("SELECT * FROM creditcardtype");
             $query->execute();
             $result = $query->fetchAll();
-
+            
+            // The syntax used in the body of foreach called herdocs syntax
             foreach($result as $k => $v) {
                 echo <<<EOF
                     <tr>
-                    <td>{$v['id']}</td>
-                    <td>{$v['creditCardType']}</td>
-                    <td><form action="<?php echo htmlspecialchars({$_SERVER["PHP_SELF"]});?>"><input type="submit" value="edit" class="table-form-button"></form></td>
-                    <td><form action='<?php echo htmlspecialchars({$_SERVER["PHP_SELF"]}); ?>'><input type="submit" value="delete" class="table-form-button"></form></td>
+                        <td>{$v['id']}</td>
+                        <td>{$v['creditCardType']}</td>
+                        <td>
+                            <form action="https://localhost/E-COMMERCE/Admin/entities/credit-card-management/manage-credit-card-type.php" method="POST">
+                                <input type="hidden" name="editedId" value="{$v['id']}">
+                                <input type="hidden" name="editedName" value="{$v['creditCardType']}">
+                                <input type="submit" name="edit" value="edit" class="table-form-button">
+                            </form>
+                        </td>
+                        <td>
+                            <form action="https://localhost/E-COMMERCE/Admin/entities/credit-card-management/manage-credit-card-type.php" method="POST">
+                                <input type="hidden" name="deletedId" value="{$v['id']}">
+                                <input type="submit" name="delete" value="delete" class="table-form-button">
+                            </form>
+                        </td>
                     </tr>
                 EOF;
             }
--- Admin/entities/credit-card-management/manage-credit-card-type.php
+++ Admin/entities/credit-card-management/manage-credit-card-type.php
@@ -1,10 +1,30 @@
 
 <?php
-    
     include "CreditcardManager.php";
-    $submitted_c_c_type = $err = $c_c_type_created = "";
+    $submitted_c_c_type = $err = $c_c_type_created = $c_c_type_edited = $err = $e_err = "";
 
-    /* code for inserting validated category data*/
+    if(!isset($editedid)) {
+        $editedid = "";
+    }
+    if(!isset($editedcctype)) {
+        $editedcctype = "";
+    }
+
+    if(isset($_POST["edit"])) {
+        // This is the id of item that you want to edit
+        $c_c_type_manager = new CreditcardManager();
+        $editedid = $c_c_type_manager->cleanData($_POST["editedId"]);
+        $editedcctype = "---> " . $c_c_type_manager->cleanData($_POST["editedName"]) . " <---";
+    }
+
+    if(isset($_POST["delete"])) {
+        // This is the id of item that you want to delete
+        $c_c_type_manager = new CreditcardManager();
+        $id = $c_c_type_manager->cleanData($_POST["deletedId"]);
+        $c_c_type_manager->deleteCreditCard($id);
+    }
+
+    /* code for inserting validated credit card type data*/
     if(isset($_POST["add-c-c-type"])) {
         $c_c_type_manager = new CreditcardManager();
 
@@ -14,12 +34,31 @@
         $submitted_c_c_type = $c_c_type_manager->cleanData($submitted_c_c_type);
 
         if(!isset($_POST["c_c_type"]) || empty($_POST["c_c_type"])) {
-            $err = "A category name is required.";
+            $err = "A credit card name is required.";
         }else if($c_c_type_manager->checkCreditCardTypeExistence($submitted_c_c_type)) {
-            $err = "This category name is already exists.";
+            $err = "This credit card name is already exists.";
         }else {
             $c_c_type_manager->insertCreditCardType($submitted_c_c_type);
-            $c_c_type_created = "Category created successfully";
+            $c_c_type_created = "Credit card type created successfully";
+            $submitted_c_c_type = "";
+        }
+    }
+
+    if(isset($_POST["edit-c-c-type"])) {
+        if(empty($_POST["e-id"])) {
+            $e_err = "Select one of the credit card types to edit it";
+        }else if(!isset($_POST["e_c_c_type"]) || empty($_POST["e_c_c_type"])) {
+            $e_err = "Please enter the new name of the selected credit card type.";
+        } else {
+            $c_c_type_manager = new CreditcardManager();
+            $id = $c_c_type_manager->cleanData($_POST["e-id"]);
+            $new_name = $c_c_type_manager->cleanData($_POST["e_c_c_type"]);
+
+            if($c_c_type_manager->editCreditCardType($id, $new_name) > 0) {
+                $c_c_type_edited = "Credit card type edited successfully";
+            } else
+                $e_err = "There's a problem.";
+            
         }
     }
 ?>
@@ -50,9 +89,10 @@
             <?php include "../../entities/left-pannel.php" ?>
 
             <div class="admin-main-layout">
-                <div id="add-cat-container">
+            <div style="width: 350px">
+                <div id="add-c-c-type-container">
                     <h2 class="main-layout-title">Add Credit Card Type</h2>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
                         <div style="display: flex">
                             <div  style="margin-bottom: 6px; margin-left: 2px; color: rgb(19, 184, 55);"><?php echo $c_c_type_created; ?></div>
                         </div>
@@ -66,6 +106,29 @@
 
                         <input type="submit" name="add-c-c-type" class="styled-button" value="Add credit card type">
                     </form>
+                </div>
+                <div id="edit-c-c-type-container">
+                        <h2 class="main-layout-title">Edit Credit Card Type</h2>
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                            <div style="display: flex">
+                                <div  style="margin-bottom: 6px; margin-left: 2px; color: rgb(19, 184, 55);"><?php echo $c_c_type_edited; ?></div>
+                            </div>
+                            <div style="display: flex">
+                                <div  style="margin-bottom: 6px; margin-left: 2px; color: rgb(218, 47, 47);"><?php echo $e_err ?></div>
+                            </div>
+                            <div style="display: flex">
+                                <label style="font-weight: bold; margin-left: 3px;" for="c_c_type">ID</label>
+                            </div>
+                            <input type="text" name="e_c_c_id" id="e_c_c_id" class="styled-form-input" value="<?php echo $editedid;?>" disabled>
+                            <input type="hidden" name="e-id" value="<?php echo $editedid; ?>">
+                            <div style="display: flex">
+                                <label style="font-weight: bold; margin-left: 3px;" for="c_c_type">New Credit Card Type Name</label>
+                            </div>
+                            <input type="text" name="e_c_c_type" id="e_c_c_type" class="styled-form-input" value="<?php echo $editedcctype;?>">
+
+                            <input type="submit" name="edit-c-c-type" class="styled-button" value="Edit credit card type">
+                        </form>
+                    </div>
                 </div>
 
                 <div id="available-c-c-types-container">
@@ -85,7 +148,6 @@
                         </table>
                     </div>
                 </div>
-                
             </div>
         </div>
     </main>
