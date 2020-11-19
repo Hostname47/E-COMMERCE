1ddf6ae8ade8426c6fe6c12397013e05ff1f4397
HOSTNAME47 <mouadstev1@gmail.com>
2020-11-19 09:48:31 +0100

    make featured product section responsive to all screens

--- index.php
+++ index.php
@@ -71,6 +71,96 @@
                         </div>
                     </div>
                 </div>
+                <div class="product-item">
+                    <!-- discount -->
+                    <div class="discount-part">
+                        <span class="discount-value">45.55</span>% OFF
+                    </div>
+                    <!-- love product -->
+                    <div class="product-like-container">
+                        <a href="#"><img class="product-like" src="images/product-assets/like.png" alt=""></a>
+                    </div>
+                    <div class="product-image-container">
+                        <a href="#"><img class="product-image" src="images/featured-product-assets/bvlgary.png" alt=""></a>
+                    </div>
+                    <hr class="line-underneath">
+                    
+                    <div class="product-info-container">
+                        <p class="product-category">Category: watches</p>
+                        <p class="product-name">Octo Finissimo Skeleton In Rose Gold With Strap</p>
+                        <p class="product-price">24,560$<span class="discounted-price">30,700$</span></p>
+                        <div class="rate-section">
+                            <div class="rate-color-container">
+                                
+                            </div>
+                            <img src="images/product-assets/rate.png" alt="rate" class="rate-stars-container">
+                            <p class="product-number-of-rates">(32)</p>
+                        </div>
+                        <div>
+                            <a href="#" class="product-add-to-cart">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="product-item">
+                    <!-- discount -->
+                    <div class="discount-part">
+                        <span class="discount-value">45.55</span>% OFF
+                    </div>
+                    <!-- love product -->
+                    <div class="product-like-container">
+                        <a href="#"><img class="product-like" src="images/product-assets/like.png" alt=""></a>
+                    </div>
+                    <div class="product-image-container">
+                        <a href="#"><img class="product-image" src="images/featured-product-assets/bvlgary.png" alt=""></a>
+                    </div>
+                    <hr class="line-underneath">
+                    
+                    <div class="product-info-container">
+                        <p class="product-category">Category: watches</p>
+                        <p class="product-name">Octo Finissimo Skeleton In Rose Gold With Strap</p>
+                        <p class="product-price">24,560$<span class="discounted-price">30,700$</span></p>
+                        <div class="rate-section">
+                            <div class="rate-color-container">
+                                
+                            </div>
+                            <img src="images/product-assets/rate.png" alt="rate" class="rate-stars-container">
+                            <p class="product-number-of-rates">(32)</p>
+                        </div>
+                        <div>
+                            <a href="#" class="product-add-to-cart">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="product-item">
+                    <!-- discount -->
+                    <div class="discount-part">
+                        <span class="discount-value">45.55</span>% OFF
+                    </div>
+                    <!-- love product -->
+                    <div class="product-like-container">
+                        <a href="#"><img class="product-like" src="images/product-assets/like.png" alt=""></a>
+                    </div>
+                    <div class="product-image-container">
+                        <a href="#"><img class="product-image" src="images/featured-product-assets/bvlgary.png" alt=""></a>
+                    </div>
+                    <hr class="line-underneath">
+                    
+                    <div class="product-info-container">
+                        <p class="product-category">Category: watches</p>
+                        <p class="product-name">Octo Finissimo Skeleton In Rose Gold With Strap</p>
+                        <p class="product-price">24,560$<span class="discounted-price">30,700$</span></p>
+                        <div class="rate-section">
+                            <div class="rate-color-container">
+                                
+                            </div>
+                            <img src="images/product-assets/rate.png" alt="rate" class="rate-stars-container">
+                            <p class="product-number-of-rates">(32)</p>
+                        </div>
+                        <div>
+                            <a href="#" class="product-add-to-cart">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
